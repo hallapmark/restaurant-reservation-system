317f1e10a5c86ad399c0487c9166346c348e1f4b
Mark Hallap <hallapmark@gmail.com>
2026-03-15 21:15:49 +0200

    Initial commit

---
+++ backend/src/main/java/com/hallap/reserveerimissysteem/ReserveerimissysteemApplication.java
@@ -0,0 +1,13 @@
+package com.hallap.reserveerimissysteem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveerimissysteemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveerimissysteemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hallap/reserveerimissysteem/ReserveerimissysteemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hallap.reserveerimissysteem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveerimissysteemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
