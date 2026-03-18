b4c8c3b4002b15679b3b89c670340adf1cd1ea4c
Mark Hallap <hallapmark@gmail.com>
2026-03-18 19:06:25 +0200

    Create Reservation Api Controller stub

--- backend/src/main/java/com/hallap/reserveerimissysteem/controller/ReservationApiController.java
+++ backend/src/main/java/com/hallap/reserveerimissysteem/controller/ReservationApiController.java
@@ -1,12 +1,41 @@
 package com.hallap.reserveerimissysteem.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class ReservationApiController {
 
+    @GetMapping("/layout")
+    public LayoutResponse getLayout() {
+        return new LayoutResponse();
+    }
+
+    @PostMapping("/availability")
+    public AvailabilityResponse checkAvailability(
+            @RequestBody AvailabilityRequest availabilityRequest
+    ) {
+        return new AvailabilityResponse();
+    }
+
+    @PostMapping("/recommendations")
+    public RecommendationsResponse getRecommendations(
+            @RequestBody RecommendationsRequest recommendationsRequest
+    ) {
+        return new RecommendationsResponse();
+    }
+
+    @PostMapping("/reservations")
+    public ReservationResponse makeReservation(
+            @RequestBody ReservationRequest reservationRequest
+    ) {
+        return new ReservationResponse();
+    }
+
+    @GetMapping("/reservations/{id}")
+    public ReservationResponse getReservation(@PathVariable String id) {
+        return new ReservationResponse();
+    }
 }
