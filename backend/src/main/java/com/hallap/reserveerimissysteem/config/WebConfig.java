d4842edd463989203c980fee7dedf08c41badbb9
Mark Hallap <hallapmark@gmail.com>
2026-03-20 10:27:40 +0200

    Add WebConfig
    All origins enabled for now. This should complete the backend scaffolding, should be able to test the endpoints now and start building out the corresponding UI

---
+++ backend/src/main/java/com/hallap/reserveerimissysteem/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.hallap.reserveerimissysteem.config;
+
+//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+//    hiljem saan siin frontend url'id injectida, praegu allow *
+//    @Value("${frontend.urls}")
+//    private String[] frontendUrls;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
